c2a300b9d7f5d5faac23b91588bcfa7ed7ad6a62
Lena Feldberg <lena@teacherspayteachers.com>
2018-01-09 15:04:54 -0500

    test happy path

--- test/nomnomsfarm_test.exs
+++ test/nomnomsfarm_test.exs
@@ -1,8 +1,29 @@
 defmodule NomNomsFarmTest do
   use ExUnit.Case
+  alias Ecto.Adapters.SQL.Sandbox
   doctest NomNomsFarm
 
-  test "greets the world" do
-    assert NomNomsFarm.hello() == :world
+  alias NomNomsFarm.{Farm, Farmer, Repo, User, UsdaFarm}
+
+  setup do
+    :ok = Sandbox.checkout(Repo)
+    Sandbox.mode(Repo, {:shared, self()})
+  end
+
+  describe "register_farm_admin/1" do
+    test "successful registers a farm admin" do
+      Repo.insert!(%UsdaFarm{name: "Bartleby & Son's", address: "400 Plainfield", usda_uid: "00920"})
+
+      args = %{
+        username: "Bob",
+        password: "password",
+        name: "bob",
+        email: "bob@bob.com",
+        usda_uid: "00920",
+        farm_name: "bob's sunflower farm"
+      }
+
+      assert :ok = NomNomsFarm.register_farm_admin(args)
+    end
   end
 end
